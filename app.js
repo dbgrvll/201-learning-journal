23eb09e051bff83701a959a53509376784e5aab9
David Gravelle <david.gravelle@gmail.com>
2019-10-29 21:25:23 -0700

    A day's work

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
 'use strict'; 
+.toUpperCase()
+<script>
+
+var visitorName = prompt ('What is your name?');
+console.log(visitorName)
+alert('Glad you stopped by ' +visitorName + '!')
+         
+var schoolName = prompt ('Have you heard of Groovy UVee? That's the school I went to.');
+console.log(schoolName)
+
+function schoolName {
+  if (schoolName.toUpperCase() === 'Yes') {
+    return "Then you've heard of the University of Vermont or UVM! That's great!";
+  } else {
+    return "You need to look into the University of Vermont or UVM!";
+  }
+}
+
+var majorName = prompt ('Can you guess what I may have studied?');
+console.log(majorName)
+
+function majorName {
+    if (majorName.toUpperCase() === 'Yes') {
+      return "Yup. That's right. TWO majors: English AND ECON, too.";
+    } else {
+      return "That was a trick question. I had TWO majors: English AND ECON, too.";
+    }
+  }
+
+var workName = prompt ('Do you know what Global Content Managers do?');
+console.log(tasteName)
+
+function workName {
+    if (workName.toUpperCase() === 'Yes') {
+      return "Yeah. We're kind of like writers, editors, strategists, and project managers, too.";
+    } else {
+      return "We're kind of like writers, editors, strategists, and project managers, too.";
+    }
+  }
+
+
+var yearsName = prompt ('How good are you for me?');
+console.log(goodName)
+
+alert('You are a ' +vegetableName + ' ' + colorName + ' ' + tasteName + ' ' + goodName)
+</script>
