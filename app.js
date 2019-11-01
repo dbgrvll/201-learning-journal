2184bb01e476d4780fbb13646c63b937e9f65642
David Gravelle <david.gravelle@gmail.com>
2019-10-31 21:07:38 -0700

    Mods to js,css, and html. also, deleted a js file I did not need

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 'use strict'; 
-.toUpperCase()
-<script>
+
+
 
 var visitorName = prompt ('What is your name?');
 console.log(visitorName)
 alert('Glad you stopped by ' +visitorName + '!')
          
-var schoolName = prompt ('Have you heard of Groovy UVee? That's the school I went to.');
+var schoolName = prompt ('Have you heard of Groovy UVee? That\'s the school I went to.');
 console.log(schoolName)
 
 function schoolName {
   if (schoolName.toUpperCase() === 'Yes') {
-    return "Then you've heard of the University of Vermont or UVM! That's great!";
+    return "Then you\'ve heard of the University of Vermont or UVM! That\'s great!";
   } else {
     return "You need to look into the University of Vermont or UVM!";
   }
@@ -22,7 +22,7 @@ console.log(majorName)
 
 function majorName {
     if (majorName.toUpperCase() === 'Yes') {
-      return "Yup. That's right. TWO majors: English AND ECON, too.";
+      return "Yup. That\'s right. TWO majors: English AND ECON, too.";
     } else {
       return "That was a trick question. I had TWO majors: English AND ECON, too.";
     }
@@ -33,15 +33,93 @@ console.log(tasteName)
 
 function workName {
     if (workName.toUpperCase() === 'Yes') {
-      return "Yeah. We're kind of like writers, editors, strategists, and project managers, too.";
+      return "Yeah. We\'re kind of like writers, editors, strategists, and project managers, too.";
     } else {
-      return "We're kind of like writers, editors, strategists, and project managers, too.";
+      return "We\'re kind of like writers, editors, strategists, and project managers, too.";
     }
   }
 
+  var workQuality = prompt ('Have heard it\'s great to be a technical writer?');
+  console.log(tasteName)
+  
+  function workQuality {
+      if (workQuality.toUpperCase() === 'Yes') {
+        return "Yeah. Then you\'ve read articles about how it\'s satisfying work. Flexible and lucrative.";
+      } else {
+        return "I guess you missed all the articles about how it\'s satisfying work. Flexible and lucrative.";
+      }
+    }
 
-var yearsName = prompt ('How good are you for me?');
-console.log(goodName)
+    var workFuture = prompt ('Do you know the long-term prospects for technical writers and other content folks?');
+    console.log(tasteName)
+    
+    function workFuture {
+        if (workFuture.toUpperCase() === 'Yes') {
+          return "Then you know prospects are neither up nor down. Just flat. Not a hot, new field.";
+        } else {
+          return "Let me tell you then. Prospects are neither up nor down. Just flat. Not a hot, new field.";
+        }
+      }
 
-alert('You are a ' +vegetableName + ' ' + colorName + ' ' + tasteName + ' ' + goodName)
-</script>
+    
+
+//As a user, I would like to be guided to an answer through a series of feedback responses so that I can learn more about the site owner.
+
+//Add a 6th question to the guessing game that takes in a numeric input by prompting the user to guess a number.
+//Indicates through an alert if the guess is “too high” or “too low”.
+//It should give the user exactly four opportunities to get the correct answer.
+//After all attempts have been exhausted, tell the user the correct answer. Consider using a loop of some sort.
+
+var workFuture = prompt ('Do you know the long-term prospects for technical writers and other content folks?');
+    console.log(tasteName)
+    
+    function workFuture {
+        if (workFuture.toUpperCase() === 'Yes') {
+          return "Then you know prospects are neither up nor down. Just flat. Not a hot, new field.";
+        } else {
+          return "Let me tell you then. Prospects are neither up nor down. Just flat. Not a hot, new field.";
+        }
+      }
+
+
+//As a user, I would like to be guided to an answer through a series of feedback responses so that I can learn more about the site owner.
+
+//Add a 6th question to the guessing game that takes in a numeric input by prompting the user to guess a number.
+//Indicates through an alert if the guess is “too high” or “too low”.
+//It should give the user exactly four opportunities to get the correct answer.
+//After all attempts have been exhausted, tell the user the correct answer. Consider using a loop of some sort.
+
+var workFuture = prompt ('Do you know the long-term prospects for technical writers and other content folks?');
+    console.log(tasteName)
+    
+    function workFuture {
+        if (workFuture.toUpperCase() === 'Yes') {
+          return "Then you know prospects are neither up nor down. Just flat. Not a hot, new field.";
+        } else {
+          return "Let me tell you then. Prospects are neither up nor down. Just flat. Not a hot, new field.";
+        }
+      }
+
+//As a user, I would like to guess the answer to a question that could have many possibilities so that I can have fun with with a guessing game.
+
+//Add a 7th question that has multiple possible correct answers that are stored in an array.
+//Give the user 6 attempts to guess the correct answer.
+//The guesses will end once the user guesses a correct answer or they run out of attempts.
+//Display all the possible correct answers to the user.
+//Consider using a loop of some sort for this question.
+
+var workFuture = prompt ('Do you know the long-term prospects for technical writers and other content folks?');
+    console.log(tasteName)
+    
+    function workFuture {
+        if (workFuture.toUpperCase() === 'Yes') {
+          return "Then you know prospects are neither up nor down. Just flat. Not a hot, new field.";
+        } else {
+          return "Let me tell you then. Prospects are neither up nor down. Just flat. Not a hot, new field.";
+        }
+      }
+
+//As a user, I would like to know my final score so that I can know how well I did.
+
+//Keep track of the total number of correct answers. At the end tell them how many they got correct out of the 7 questions asked.
+//In addition to the user stories stated above, continue to use console.logs throughout the code to track the questions that are being asked to the user, and add addtional CSS to style your HTML page.
