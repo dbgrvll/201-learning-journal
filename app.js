3bb47d0cbfe52ad3637a82736cabea18b17b284e
Jin Woo Kim <jinwoov@gmail.com>
2019-11-02 13:37:18 -0700

    added function sign

--- app.js
+++ app.js
@@ -2,36 +2,36 @@
 
 
 
-var visitorName = prompt ('What is your name?');
-console.log(visitorName)
-alert('Glad you stopped by ' +visitorName + '!')
+// var visitorName = prompt ('What is your name?');
+// console.log(visitorName)
+// alert('Glad you stopped by ' +visitorName + '!')
          
-var schoolName = prompt ('Have you heard of Groovy UVee? That\'s the school I went to.');
-console.log(schoolName)
+// var schoolName = prompt ('Have you heard of Groovy UVee? That\'s the school I went to.');
+// console.log(schoolName)
 
 
-  if (schoolName.toUpperCase() === 'Yes') {
-    console.log( "Then you\'ve heard of the University of Vermont or UVM! That\'s great!");
-  } else {
-    return "You need to look into the University of Vermont or UVM!";
-  }
+//   if (schoolName.toUpperCase() === 'Yes') {
+//     console.log( "Then you\'ve heard of the University of Vermont or UVM! That\'s great!");
+//   } else {
+//     return "You need to look into the University of Vermont or UVM!";
+//   }
 
 
-var majorName = prompt ('Can you guess what I may have studied?');
-console.log(majorName)
+// var majorName = prompt ('Can you guess what I may have studied?');
+// console.log(majorName)
 
 
-    if (majorName.toUpperCase() === 'Yes') {
-      return "Yup. That\'s right. TWO majors: English AND ECON, too.";
-    } else {
-      return "That was a trick question. I had TWO majors: English AND ECON, too.";
-    }
+//     if (majorName.toUpperCase() === 'Yes') {
+//       return "Yup. That\'s right. TWO majors: English AND ECON, too.";
+//     } else {
+//       return "That was a trick question. I had TWO majors: English AND ECON, too.";
+//     }
   
 
 var workName = prompt ('Do you know what Global Content Managers do?');
 console.log(tasteName)
 
-function workName {
+function workName() {
     if (workName.toUpperCase() === 'Yes') {
       return "Yeah. We\'re kind of like writers, editors, strategists, and project managers, too.";
     } else {
@@ -39,27 +39,27 @@ function workName {
     }
   }
 
-  var workQuality = prompt ('Have heard it\'s great to be a technical writer?');
-  console.log(tasteName)
+  // var workQuality = prompt ('Have heard it\'s great to be a technical writer?');
+  // console.log(tasteName)
   
-  function workQuality {
-      if (workQuality.toUpperCase() === 'Yes') {
-        return "Yeah. Then you\'ve read articles about how it\'s satisfying work. Flexible and lucrative.";
-      } else {
-        return "I guess you missed all the articles about how it\'s satisfying work. Flexible and lucrative.";
-      }
-    }
+  // function workQuality {
+  //     if (workQuality.toUpperCase() === 'Yes') {
+  //       return "Yeah. Then you\'ve read articles about how it\'s satisfying work. Flexible and lucrative.";
+  //     } else {
+  //       return "I guess you missed all the articles about how it\'s satisfying work. Flexible and lucrative.";
+  //     }
+  //   }
 
-    var workFuture = prompt ('Do you know the long-term prospects for technical writers and other content folks?');
-    console.log(tasteName)
+  //   var workFuture = prompt ('Do you know the long-term prospects for technical writers and other content folks?');
+  //   console.log(tasteName)
     
-    function workFuture {
-        if (workFuture.toUpperCase() === 'Yes') {
-          return "Then you know prospects are neither up nor down. Just flat. Not a hot, new field.";
-        } else {
-          return "Let me tell you then. Prospects are neither up nor down. Just flat. Not a hot, new field.";
-        }
-      }
+  //   function workFuture {
+  //       if (workFuture.toUpperCase() === 'Yes') {
+  //         return "Then you know prospects are neither up nor down. Just flat. Not a hot, new field.";
+  //       } else {
+  //         return "Let me tell you then. Prospects are neither up nor down. Just flat. Not a hot, new field.";
+  //       }
+  //     }
 
     
 
